6d36f0ef05002ac36e94287f93d19656693c4185
Isaac <isaaclatta73@gmail.com>
2026-04-28 21:56:38 -0700

    wip

--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,8 @@
 use std::io;
 use std::io::{Read, Write};
 use std::net;
+use std::sync::mpsc;
+use std::sync::mpsc::Receiver;
 
 pub struct Config {
     pub bind_addr: String,
@@ -29,31 +31,113 @@ pub fn try_parse_cli_args(mut args: impl Iterator<Item = String>) -> Result<Conf
     Ok(Config { bind_addr, port })
 }
 
-fn handle_client(mut stream: net::TcpStream) -> io::Result<()> {
-    let mut buffer = [0u8; 4096];
+pub fn run_server(config: &Config) -> io::Result<()> {
+    let host_addr = format!("{}:{}", config.bind_addr, config.port);
+    let listener = net::TcpListener::bind(host_addr)?;
 
-    loop {
-        let bytes_read = stream.read(&mut buffer)?;
-        if bytes_read == 0 {
-            println!("Client disconnected");
-            break;
-        }
+    let (transmitter, receiver) = mpsc::channel::<(net::TcpStream, net::SocketAddr)>();
 
-        let message = String::from_utf8_lossy(&buffer[..bytes_read]);
+    let accept_thread = std::thread::spawn(move || {
+        return accept_loop(listener, move |stream, addr| {
+             match transmitter.send((stream, addr)) {
+                 Ok(_) => {},
+                 Err(e) => {
+                    eprintln!("Failed to send accepted client: {}", e);
+                 }
+             }
+        });
+    });
 
-        println!("Received: {}", message);
-        stream.write_all(&buffer[..bytes_read])?;
-    }
+    let poller_thread = std::thread::spawn(move || {
+         return poll_loop(receiver);
+    });
 
+    accept_thread.join().unwrap()?;
+    poller_thread.join().unwrap()?;
     Ok(())
 }
 
-pub fn run_server(config: &Config) -> io::Result<()> {
-    let listener = net::TcpListener::bind(format!("{}:{}", config.bind_addr, config.port))?;
+struct Client {
+    sock: net::TcpStream,
+    addr: net::SocketAddr,
 
-    for stream in listener.incoming() {
-        handle_client(stream?)?;
+    rx_length: usize,
+    rx_buffer: [u8; 4096],
+
+    tx_length: usize,
+    tx_buffer: [u8; 4096],
+}
+
+fn accept_loop<AcceptCallback>(listener: net::TcpListener, mut on_accept: AcceptCallback) -> io::Result<()>
+where
+    AcceptCallback: FnMut(net::TcpStream, net::SocketAddr) -> ()
+{
+    loop {
+        let (sock, addr) = match listener.accept() {
+            Ok((sock, addr)) => (sock, addr),
+            Err(e) if is_fatal(e.kind()) => return Err(e),
+            Err(e) => {
+                eprintln!("accept error: {}", e);
+                continue;
+            }
+        };
+        on_accept(sock, addr);
+    }
+}
+
+fn do_read(client: &mut Client) -> io::Result<bool> {
+    loop {
+        let bytes_read = match client.sock.read(&mut client.rx_buffer[client.rx_length..]) {
+            Ok(0) => return Ok(false),
+            Ok(n_bytes) => {
+                client.rx_length = client.rx_length + n_bytes;
+            }
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => {
+                break;
+            },
+            Err(e) if e.kind() == io::ErrorKind::Interrupted => {
+                continue;
+            },
+            Err(e) => return Err(e)
+        };
     }
 
-    Ok(())
+    Ok(true)
+}
+
+fn handle_client(client: &mut Client) -> io::Result<bool> {
+    if !do_read(client)? {
+        return Ok(false)
+    }
+
+    Ok(true)
+}
+
+fn new_client(sock: net::TcpStream, addr: net::SocketAddr) -> Client {
+    return Client { sock, addr, rx_length: 0, rx_buffer: [0u8; 4096], tx_length: 0, tx_buffer: [0u8; 4096] };
+}
+
+fn poll_loop(receiver: Receiver<(net::TcpStream, net::SocketAddr)>) -> io::Result<()> {
+    let mut clients: Vec<Client> = Vec::new();
+
+    loop {
+        while let Ok((sock, addr)) = receiver.try_recv() {
+            clients.push(new_client(sock, addr));
+        }
+
+        for client in clients.iter_mut() {
+            if !do_read(client)? {
+                clients.remove(client);
+                continue;
+            }
+
+
+        }
+
+        std::thread::yield_now();
+    }
+}
+
+fn is_fatal(error: io::ErrorKind) -> bool {
+    return true;
 }
